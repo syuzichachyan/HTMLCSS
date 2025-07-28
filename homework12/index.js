4717d6279bc3bd604bd9592101f569b99ff7d2c7
syuzanna.chachyan <kchachyan@gmail.com>
2025-07-28 14:49:28 +0400

    done

--- homework12/index.js
+++ homework12/index.js
@@ -1,6 +1,24 @@
-const hamburger = document.getElementsByClassName('hamburger')?.[0];
-const mobileNav = document.getElementsByClassName('mobile-nav')?.[0];
+const initNavBar = () => {
 
-hamburger.addEventListener('click', () => {
-    mobileNav.classList.toggle('hidden');
-});
+    const hamburger = document.getElementsByClassName('hamburger')?.[0];
+    const mobileNav = document.getElementsByClassName('mobile-nav')?.[0];
+
+    hamburger.addEventListener('click', () => {
+        mobileNav.classList.toggle('hidden');
+    });
+
+}
+
+const initSlide = () => {
+    new Swiper(".mySwiper", {
+        slidesPerView: 'auto',
+        spaceBetween: 29,
+        freeMode: true,
+        pagination: {
+            clickable: true,
+        },
+    });
+}
+
+initSlide()
+initNavBar()
